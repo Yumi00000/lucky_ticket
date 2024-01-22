5c7866173b6708bdf9f94e70307c94f1ba5f5b26
yumi <vb34550@gmail.com>
2024-01-22 16:10:43 +0100

    Mow it's counting lucky tickets

--- lucky_ticket.py
+++ lucky_ticket.py
@@ -2,36 +2,35 @@ import threading
 from queue import Queue
 
 
-def process_worker(data_chunk, results_queue, semaphore):
+def process_worker(ticket, results_queue, semaphore):
     with semaphore:
-        result = process_data(data_chunk)
+        result = process_ticket(ticket)
         results_queue.put(result)
 
 
-def process_data(data_chunk):
-    data_chunk = str(data_chunk).zfill(6)
-
-    first_numb, second_numb, third_numb, fourth_numb, fifth_numb, sixth_numb = map(int, data_chunk)
-
+def process_ticket(ticket):
+    ticket_str = str(ticket).zfill(6)
+    first_numb, second_numb, third_numb, fourth_numb, fifth_numb, sixth_numb = map(int, ticket_str)
     return first_numb + second_numb + third_numb == fourth_numb + fifth_numb + sixth_numb
 
 
 if __name__ == "__main__":
-    data = int(input("Enter number: "))
+    data = []
+    for i in range(11):
+        data.append(int(input("Enter ticket number: ")))
+
     results_queue = Queue()
     semaphore = threading.Semaphore(4)
 
-    chunk_size = max(len(str(data)) // 4, 6)
-    data_chunks = [int(str(data)[i:i + chunk_size]) for i in range(0, len(str(data)), chunk_size)]
-
     threads = []
-    for chunk in data_chunks:
-        thread = threading.Thread(target=process_worker, args=(chunk, results_queue, semaphore))
+    for ticket in data:
+        thread = threading.Thread(target=process_worker, args=(ticket, results_queue, semaphore))
         thread.start()
         threads.append(thread)
 
     for thread in threads:
         thread.join()
 
-    results = [results_queue.get() for _ in range(len(data_chunks))]
-    print(results[0])
+    results = [results_queue.get() for _ in range(len(data))]
+
+    print(f"Numb of Lucky tickets: {results.count(True)}")
