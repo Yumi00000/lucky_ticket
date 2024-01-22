c30e7288054ca4600a51f922a5c271bde44dbe69
yumi <vb34550@gmail.com>
2024-01-22 18:45:00 +0100

    Mow it with 4 threads

--- lucky_ticket.py
+++ lucky_ticket.py
@@ -1,27 +1,32 @@
 import threading
 from queue import Queue
 
-
-def process_worker(ticket, results_queue, semaphore):
-    with semaphore:
-        result = process_ticket(ticket)
-        results_queue.put(result)
-
+def process_worker(ticket_range, results_queue, semaphore):
+    for ticket in ticket_range:
+        with semaphore:
+            result = process_ticket(ticket)
+            results_queue.put(result)
 
 def process_ticket(ticket):
     ticket_str = str(ticket).zfill(6)
     first_numb, second_numb, third_numb, fourth_numb, fifth_numb, sixth_numb = map(int, ticket_str)
     return first_numb + second_numb + third_numb == fourth_numb + fifth_numb + sixth_numb
 
-
 if __name__ == "__main__":
     data = [format(i, '06d') for i in range(1000000)]
     results_queue = Queue()
     semaphore = threading.Semaphore(4)
 
+    num_threads = 4
+    chunk_size = len(data) // num_threads
+
     threads = []
-    for ticket in data:
-        thread = threading.Thread(target=process_worker, args=(ticket, results_queue, semaphore))
+    for i in range(num_threads):
+        start_index = i * chunk_size
+        end_index = (i + 1) * chunk_size if i < num_threads - 1 else len(data)
+        thread_data = data[start_index:end_index]
+
+        thread = threading.Thread(target=process_worker, args=(thread_data, results_queue, semaphore))
         thread.start()
         threads.append(thread)
 
@@ -30,4 +35,4 @@ if __name__ == "__main__":
 
     results = [results_queue.get() for _ in range(len(data))]
 
-    print(f"Numb of Lucky tickets: {results.count(True)}")
+    print(f"Number of Lucky tickets: {results.count(True)}")
